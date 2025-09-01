c7d466f686bea09747ded8418e4a1beeaa592ec1
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-09-01 13:11:20 +0900

    とりあえずスタイリングはこれで完了
      - お手本と違って背景が白だが、これは CSS に TailwindCSS のインポート以外何も書いていないためだと思われる
      - 一列しか表示されていないが、これは API を取得するときの TMB 側での設定操作をミスったのかもしれない
      - しかし以上は今回やることの本質ではないような気がする。また、時間の余裕もあまりない。そこで、今回はこのまま進めていくことにする。

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { requests } from './request';
 function App() {
   return (
     <div className='App'>
-      <p className='bg-red-500'>TailWindCSS テスト</p>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} /> {/* title プロパティが必要なようなので追加 */}
     </div>
   )
