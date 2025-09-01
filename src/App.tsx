629b91161836087d7780d10fc15f7aadd021b937
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-09-01 15:41:56 +0900

    Netflix Original の行しか表示されなかった問題の解決
      - Row コンポーネントを App 内で1行しか記述していなかったためだった
      - API キー生成時にミスをしていたわけではない
      - 背景を黒にしておく方法を知らないので、とりあえずテキストは黒にしておく

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className='App'>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} /> {/* title プロパティが必要なようなので追加 */}
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   )
 }
